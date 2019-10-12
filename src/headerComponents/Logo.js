aa866eca1b0c29e90f08c6f265bd4bcd9e1eeac5
SwanHub <jdprince555@gmail.com>
2019-10-12 17:30:42 -0600

    new icon

--- src/headerComponents/Logo.js
+++ src/headerComponents/Logo.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default function Logo(props) {
+export default class Logo extends React.Component {
     
-    const handleClick = () => {
-        props.resetMain()
+    state = {
+        active: false
     }
 
-    return (
-        <NavLink to="/">
-            <div className="logo" onClick={handleClick}>
-                    <img src={require('../images/shield.png')} alt="DataTrust Shield Logo"/>
-                    <span id="name-data">Data</span><span>Trust</span>
-            </div>
-        </NavLink>
-    )
+    toggleActive = () => {
+        this.setState({
+            active: true
+        })
+    }
+    
+    toggleInactive = () => {
+        this.setState({
+            active: false
+        })
+    }
+
+    handleClick = () => {
+        this.props.resetMain()
+    }
+    render(){
+        return (
+            <NavLink to="/">
+                <div className="logo" 
+                        onClick={this.handleClick}
+                        onMouseEnter={this.toggleActive}
+                        onMouseLeave={this.toggleInactive}>
+                    <img src={require('../images/dt-icon.png')} alt="DataTrust Shield Logo"/>
+                    
+                    <div>
+                        <span id="name-data">Data</span><span id="name-trust">Trust</span> <br />
+                        <span className={this.state.active ? "logo-show" : "logo-hide"}> 
+                            Data Transparency By Popular Demand
+                        </span>
+                    </div>
+                </div>
+            </NavLink>
+        )
+    }
 }
